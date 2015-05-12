316e956fb55a7430b11f5eebadb8308ec273b4e5
unknown <amohapat@nbhydamohapat7.bedford.progress.com>
2015-05-12 08:38:02 +0530

    Add logging in demo method

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = function(width, height) {
+  Console.log("npm demo");
   return width * height;
 };
+
+module.exports = function(width, height,depth) {
+  Console.log("npm demo");
+  return width * height * depth;
+};
+
